908e30d32563abac17718beba90221f2a3fd7b25
Janne Turunen <jannesanteriturunen@live.com>
2018-03-07 10:16:11 +0200

    Added player,enemy base class, and simple temp animation for player attack

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float moveSpeed = 0.0f;
+    public float hitPoints = 0.0f;
+    public float attackSpeed = 0.0f;
+
+    bool facingRight = true;
+    public bool isControlled = false;
+
+    Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        //TODO: get real inputs
+        /*if (Input.GetButton("Fire2"))
+        {
+            Attack();
+        }*/
+
+        //TODO: Get real inputs
+        /* if (Input.GetButton("Fire2"))
+         {
+            Defensive();
+         }*/
+
+        //TODO: Get real inputs
+        /*if (Input.GetButton("Fire3"))
+        {
+           SpecialAttack();
+        }*/
+
+    }
+
+    private void Move()
+    {
+        if (isControlled)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+            GetComponent<Rigidbody2D>().velocity = movement * moveSpeed;
+
+            if (moveHorizontal > 0 && !facingRight)
+            {
+                Flip();
+            }
+            else if (moveHorizontal < 0 && facingRight)
+            {
+                Flip();
+            }
+        } else
+        {
+            float step = moveSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+        }
+    }
+
+    private void Attack()
+    {
+        if (isControlled)
+        {
+            //TODO: Get real inputs
+            if (Input.GetButton("Fire1"))
+            {
+                Debug.Log("Enemy Attacks");
+            }
+        }
+    }
+
+    private void Defensive()
+    {
+        Debug.Log("Enemy Defence");
+    }
+
+    private void SpecialAttack()
+    {
+        Debug.Log("Enemy Special Attacks");
+    }
+
+    private void Flip()
+    {
+        Debug.Log("Flip");
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Animator anim;
+
+    public float moveSpeed = 0.0f;
+    public float hitPoints = 0.0f;
+    public float damageDealt = 0.0f;
+    public float victoryPoints = 0;
+    //public Weapon usedWeapon = null; weapon that is used by the player
+    float moveHorizontal;
+    float moveVertical;
+    bool facingRight = true;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        Move();
+        Attack();
+        Defensive();
+    }
+
+    private void Move()
+    {
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        GetComponent<Rigidbody2D>().velocity = movement * moveSpeed;
+
+        if (moveHorizontal > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (moveHorizontal < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void Attack ()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (moveVertical > 0)
+            {
+                Debug.Log("Player Attacks Up");
+                anim.SetTrigger("Attack");
+            } else
+            {
+                Debug.Log("Player Attacks");
+                anim.SetTrigger("Attack");
+            }
+
+        }
+    }
+
+    private void Defensive()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            Debug.Log("Player Defence");
+        }
+    }
+
+    private void Flip()
+    {
+        Debug.Log("Flip");
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
