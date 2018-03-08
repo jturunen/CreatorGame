424e3c9b9f6c28280d211764e25210e5368d238d
Janne Turunen <jannesanteriturunen@live.com>
2018-03-08 15:04:23 +0200

    Added inputs for creation, created spawn controller with monster lists

---
+++ Assets/Scripts/MonsterList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterList {
+
+    public List<GameObject> allMonsters { get; set; }
+                                       
+    // Use this for initialization
+    public MonsterList() {
+        allMonsters = new List<GameObject>();
+        for (int i = 0; i <= 8; i++)
+        {
+            allMonsters.Add(new GameObject());
+        }
+    }
+	
+
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpawnController : MonoBehaviour {
+
+    /*
+     * Spawn enemies from list:
+     * When button is pressed, monster is saved to list with position.
+     * Then for example, when players enter room, go through list and spawn monsters then.
+     */
+
+    public List<Vector2> positionList = null;
+
+    public List<GameObject> monsterList = null;
+
+    public MonsterList allMonsters;
+
+    public List<GameObject> placeableEnemies;
+
+    public int maxUnits = 0;
+    public int currentUnit = 0;
+    private bool monsterSpawning = false;
+
+    public float spawnRate = 0.5F;
+    private float nextSpawn = 0.0F;
+
+    // Use this for initialization
+    void Start () {
+        positionList = new List<Vector2>();
+        monsterList = new List<GameObject>();
+
+        allMonsters = new MonsterList();
+
+        placeableEnemies = allMonsters.allMonsters;
+        
+        for (int i = 0; i <= maxUnits; i++)
+        {
+            placeableEnemies.Add(new GameObject());
+            positionList.Add(new Vector2(i, i));
+            //Debug.Log(positionList[i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentUnit <= maxUnits && Time.time > nextSpawn) { 
+            if (Input.GetButton("Fire1"))
+            {
+                Debug.Log("A button pressed");
+
+                SpawnToPoint(placeableEnemies[0]);
+            }
+            if (Input.GetButton("Fire2"))
+            {
+                Debug.Log("B button pressed");
+                SpawnToPoint(placeableEnemies[1]);
+            }
+            if (Input.GetButton("Fire3"))
+            {
+                Debug.Log("X button pressed");
+                SpawnToPoint(placeableEnemies[2]);
+            }
+            if (Input.GetButton("Fire4"))
+            {
+                Debug.Log("Y button pressed");
+                SpawnToPoint(placeableEnemies[3]);
+            }
+            if (Input.GetButton("Left Bumber"))
+            {
+                Debug.Log("Left bumber pressed");
+                SpawnToPoint(placeableEnemies[4]);
+            }
+            if (Input.GetButton("Right Bumber"))
+            {
+                Debug.Log("Right bumber pressed");
+                SpawnToPoint(placeableEnemies[5]);
+            }
+        } else if (currentUnit == maxUnits && !monsterSpawning)
+        {
+            // Spawn enemies from list - move this to room controller?
+            SpawnUnitsToPoints();
+        }
+    }
+
+    private void SpawnToPoint(GameObject chosenEnemy)
+    {
+        nextSpawn = Time.time + spawnRate;
+
+        chosenEnemy.transform.position = positionList[currentUnit];
+        monsterList.Add(chosenEnemy);
+
+        currentUnit++;
+    }
+
+    private void SpawnUnitsToPoints()
+    {
+        foreach (GameObject monster in monsterList)
+        {
+            Instantiate(monster, monster.transform.position, Quaternion.identity);
+        }
+        monsterSpawning = true;
+    }
+}
+
+
