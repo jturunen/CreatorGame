9794e06f3f4be266cebd6859e33fdc17ff9d8e57
Janne Turunen <jannesanteriturunen@live.com>
2018-04-04 17:58:54 +0300

    Selected minions is put to list, and temporary spawned immidiately

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -4,45 +4,56 @@ using UnityEngine;
 
 
 public class SpawnController : MonoBehaviour {
-     
-     /*
-     * Spawn enemies from list:
-     * When button is pressed, monster is saved to list with position.
-     * Then for example, when players enter room, go through list and spawn monsters then.
-     */
 
-    public List<GameObject> chosenMonstersList = null;
+    /*
+    * Spawn enemies from list:
+    * When button is pressed, monster is saved to list with position.
+    * Then for example, when players enter room, go through list and spawn monsters then.
+    */
+    #region Variables
 
-   // public MonsterList allMonsters;
-    public GameObject gameController;
-    public LevelController levelController;
+    public int maxUnits = 0; //How many units can be spawned to map
+    private int currentUnit = 0; // Which unit(slot) is currently being used 
+    private bool minionSpawning = false; //Check to prevent multiple minion spawns
 
-    public List<Vector2> positionList;
-    public List<GameObject> placeableEnemies;
+    public float spawnRate = 0.5F; // How fast can be minions put to list (spawned to list)
+    private float nextSpawn = 0.0F; 
 
-    private GameObject spawnPointToFind;
+    private List<GameObject> chosenMinionsList = null; //List where chosen minions are put
 
-    public int maxUnits = 0;
-    public int currentUnit = 0;
-    private bool monsterSpawning = false;
+    private List<GameObject> minionsFromPrefabs = new List<GameObject>(); // List for minions from prefabs
 
-    public float spawnRate = 0.5F;
-    private float nextSpawn = 0.0F;
+    //Spawnpoint variables
+    private List<Vector2> spawnPointPositions = new List<Vector2>(); //List for spawnpoint positions
+    public GameObject spawnPointPrefab; //Prefab for spawnpoint
+    private GameObject spawnPointObject; //Temporary spawnpoint object to name it on instantiate
+    private GameObject spawnPointToFind; //Temporary spawnpoint object to find it on scene
+
+    #endregion
 
     // Use this for initialization
     void Start() {
 
-        chosenMonstersList = new List<GameObject>();
-        placeableEnemies = new List<GameObject>();
+        // Get Minions folder as object.
+        Object[] subListObjects = Resources.LoadAll("Prefabs/Characters", typeof(GameObject));
 
-        placeableEnemies = gameController.GetComponent<MonsterList>().allMonsters;
+        // Get all minion prefabs from folder "Minions" and put to list
+        foreach (GameObject subListObject in subListObjects)
+        {
+            GameObject listObject = subListObject;
+            Debug.Log("Created minion: " + listObject);
+            minionsFromPrefabs.Add(listObject);
+        }
 
-        Debug.Log("placeableEnemies" + placeableEnemies);
-      //  Debug.Log("allMonsters.allMonsters " + allMonsters.allMonsters);
+        //Create spawnpoints for level minimap
+        //Positions are found in spawnPointPositions list
+        createSpawnPoints();
 
-        positionList = levelController.spawnPoints;
-        //changeSpawnPointColor(toUsed: false);
+        //Initialize list for minions that will be chosen to the level
+        chosenMinionsList = new List<GameObject>();
 
+        //Set first spawnpoint color to be next used.
+        changeSpawnPointColor(toUsed: false);
     }
 
     // Update is called once per frame
@@ -51,31 +62,33 @@ public class SpawnController : MonoBehaviour {
         if (currentUnit <= maxUnits && Time.time > nextSpawn) { 
             if (Input.GetButton("Fire1"))
             {
-                SpawnToPoint(placeableEnemies[0]);
+                Debug.Log("Created minion Fire1: " + minionsFromPrefabs[0]);
+                SpawnToPoint(minionsFromPrefabs[0]);
             }
             if (Input.GetButton("Fire2"))
             {
-                SpawnToPoint(placeableEnemies[1]);
+                SpawnToPoint(minionsFromPrefabs[1]);
             }
             if (Input.GetButton("Fire3"))
             {
-                SpawnToPoint(placeableEnemies[2]);
+                SpawnToPoint(minionsFromPrefabs[2]);
             }
             if (Input.GetButton("Fire4"))
             {
-                SpawnToPoint(placeableEnemies[3]);
+                SpawnToPoint(minionsFromPrefabs[3]);
             }
-            if (Input.GetButton("Left Bumber"))
+            /*if (Input.GetButton("Left Bumber"))
             {
-                SpawnToPoint(placeableEnemies[4]);
+                SpawnToPoint(minionsFromPrefabs[4]);
             }
             if (Input.GetButton("Right Bumber"))
             {
-                SpawnToPoint(placeableEnemies[5]);
-            }
-        } else if (currentUnit == maxUnits && !monsterSpawning)
+                SpawnToPoint(minionsFromPrefabs[5]);
+            }*/
+        }
+        // Spawn enemies from list
+        if (currentUnit == maxUnits && !minionSpawning)
         {
-            // Spawn enemies from list - move this to room controller?
             SpawnUnitsToPoints();
         }
     }
@@ -83,37 +96,66 @@ public class SpawnController : MonoBehaviour {
     private void SpawnToPoint(GameObject chosenEnemy)
     {
         nextSpawn = Time.time + spawnRate;
-        chosenMonstersList.Add(chosenEnemy);
+        //Add chosen enemy to list.
+        chosenMinionsList.Add(chosenEnemy);
+        //Change "current" spawnpoint to used
         changeSpawnPointColor(toUsed: true);
         currentUnit++;
-        if(currentUnit < maxUnits)
+        //Change next spawnpoint to be current
+        if (currentUnit < maxUnits)
         {
             changeSpawnPointColor(toUsed: false);
         }
     }
 
+    //This is activated when the camera moves to the fight phase.
     private void SpawnUnitsToPoints()
     {
-        for (int i = 0; i < chosenMonstersList.Count; i++)
+        //go throught the list, and instantiate to them to the spawning point
+        for (int i = 0; i < chosenMinionsList.Count; i++)
         {
-            spawnPointToFind = GameObject.Find("SpawnPoint" + i);
-            Destroy(spawnPointToFind);
-            Instantiate(chosenMonstersList[i], positionList[i], Quaternion.identity);
+            Instantiate(chosenMinionsList[i], spawnPointPositions[i], Quaternion.identity);
         }
-        monsterSpawning = true;
+        //Enable minionSpawning so to prevent update to spawn enemies multiple times
+        minionSpawning = true;
     }
 
     private void changeSpawnPointColor(bool toUsed)
     {
+        //Find current spawnpoint from the scene and access its sprite
         spawnPointToFind = GameObject.Find("SpawnPoint" + currentUnit);
         SpriteRenderer spawnPointSprite = spawnPointToFind.GetComponent<SpriteRenderer>();
         if (toUsed)
         {
+            //Change to used, (black)
             spawnPointSprite.color = new Color(0f, 0f, 0f, 1f);
         } else
         {
+            //Change to next, (red)
             spawnPointSprite.color = new Color(1f, 0.5f, 0.5f, 1f);
         }
 
+    }
+
+    //Temporary solution, until we get real positions that changes with maps.
+    private void createSpawnPoints()
+    {
+        //Creating temporary list, the positions must be placed better
+        spawnPointPositions.Add(new Vector2(-7.5f, -2));
+        spawnPointPositions.Add(new Vector2(-5.5f, -2));
+        spawnPointPositions.Add(new Vector2(-3.5f, -2));
+        spawnPointPositions.Add(new Vector2(-7.5f, -4));
+        spawnPointPositions.Add(new Vector2(-5.5f, -4));
+        spawnPointPositions.Add(new Vector2(-3.5f, -4));
+
+        //Go thrgouh given coordiatens and instatiate a spawnpoint to that location
+        for (int i = 0; i < maxUnits; i++)
+        {
+            spawnPointObject = Instantiate(spawnPointPrefab, spawnPointPositions[i], Quaternion.identity);
+            //give spawnpoint a name with the number, to indentify
+            spawnPointObject.name = "SpawnPoint" + i;
+            Debug.Log("Created SPAWNPOINT: " + i);
+
+        }
     }
 }
